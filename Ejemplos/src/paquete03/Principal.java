3a49791414ae575b414f90f69d9ba23686e5c855
jhordycamacas <jhordycamacas9@gmail.com>
2025-01-16 11:34:16 -0500

    cam3

--- Ejemplos/src/paquete03/Principal.java
+++ Ejemplos/src/paquete03/Principal.java
@@ -1,4 +1,4 @@
-/*
+/*/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,6 +8,7 @@ package paquete03;
 import paquete04.OperadorUno;
 import paquete04.OperadorDos;
 import paquete05.OperadorTres;
+import paquete06.Imprimir;
 
 // import paquete04.*;
 /**
@@ -20,13 +21,8 @@ public class Principal {
         int valorA = OperadorUno.obtenerNumero(100);
         int valorB = OperadorUno.obtenerNumero(10);
         int suma = OperadorDos.obtenerSuma(valorA, valorB);
-        int multiplicacion = OperadorTres.obtenerMultiplicacion(valorA, 
-                                                                valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n"
-                + "La multiplicacion de %d * %d es igual a: %d\n",
-                valorA,valorB,
-                suma,
-                valorA, valorB,
-                multiplicacion);
+        int multiplicacion = OperadorTres.obtenerMultiplicacion(valorA, valorB);
+        Imprimir.imprimirMensaje(valorA, valorB, suma, multiplicacion);
+
     }
 }
---
+++ Ejemplos/src/paquete06/Imprimir.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete06;
+
+/**
+ *
+ * @author utpl
+ */
+public class Imprimir {
+
+    public static void imprimirMensaje(int valorA, int valorB, int suma, int multiplicacion) {
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                valorA, valorB,
+                suma,
+                valorA, valorB,
+                multiplicacion);
+
+    }
+
+}
